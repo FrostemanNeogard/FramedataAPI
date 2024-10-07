c3e7d49e935efcb4baa370143ed49baf3cf12b98
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-07 21:42:37 +0200

    Add new resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { FramedataModule } from './framedata/framedata.module';
 import { CharacterCodeController } from './characterCode/characterCode.controller';
 import { CharacterCodeService } from './characterCode/characterCode.service';
 import { CharacterNameFormatterModule } from './characterCode/characterCode.module';
+import { GameCodeModule } from './gameCode/gameCode.module';
+import { GameCodeService } from './gameCode/gameCode.service';
 
 @Module({
-  imports: [FramedataModule, CharacterNameFormatterModule],
+  imports: [FramedataModule, CharacterNameFormatterModule, GameCodeModule],
   controllers: [FramedataController, CharacterCodeController],
-  providers: [FramedataService, CharacterCodeService],
+  providers: [FramedataService, CharacterCodeService, GameCodeService],
 })
 export class AppModule {}
