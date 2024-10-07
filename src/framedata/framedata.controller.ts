70e360586252158b03caad52645024a088057f0a
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-07 22:30:23 +0200

    Refactor framedata controller

--- src/framedata/framedata.controller.ts
+++ src/framedata/framedata.controller.ts
@@ -4,31 +4,61 @@ import {
   Body,
   Logger,
   BadRequestException,
+  Get,
+  Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { FramedataService } from './framedata.service';
 import { FramedataRequestDto } from 'src/__dtos/frameDataDto';
+import { GameCode } from 'src/__types/gameCode';
+import { CharacterCodesService } from 'src/characterCodes/characterCodes.service';
 
 @Controller('framedata')
 export class FramedataController {
   private readonly logger = new Logger();
 
-  constructor(private readonly framedataService: FramedataService) {}
+  constructor(
+    private readonly framedataService: FramedataService,
+    private readonly characterCodesService: CharacterCodesService,
+  ) {}
+
+  @Get(':gameCode/:characterCode')
+  public async getFramedataForCharacter(
+    @Param('gameCode') gameCode: GameCode,
+    @Param('characterCode') characterName: string,
+  ) {
+    const characterCode = this.characterCodesService.getCharacterCode(
+      characterName,
+      gameCode,
+    );
+
+    if (characterCode == null) {
+      this.logger.log(
+        `Couldn't find character code for: ${characterName} in game: ${gameCode}`,
+      );
+      throw new NotFoundException();
+    }
+
+    this.logger.log(
+      `Returning all character data for character: ${characterName} in game: ${gameCode}`,
+    );
+
+    return await this.framedataService.getCharacterFrameData(
+      characterCode,
+      gameCode,
+    );
+  }
 
   @Post()
-  async getFrameDataSingle(@Body() frameDataDto: FramedataRequestDto) {
+  public async getFrameDataSingle(@Body() frameDataDto: FramedataRequestDto) {
+    this.logger.log('');
+
     try {
-      if (frameDataDto.input) {
-        return await this.framedataService.getSingleMoveFrameData(
-          frameDataDto.characterCode,
-          frameDataDto.gameCode,
-          frameDataDto.input,
-        );
-      } else {
-        return await this.framedataService.getCharacterFrameData(
-          frameDataDto.characterCode,
-          frameDataDto.gameCode,
-        );
-      }
+      return await this.framedataService.getSingleMoveFrameData(
+        frameDataDto.characterCode,
+        frameDataDto.gameCode,
+        frameDataDto.input,
+      );
     } catch (error) {
       return new BadRequestException(`${error}`);
     }
