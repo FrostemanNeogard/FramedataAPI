4030d4a9920e73104e06aac4e7d5f4a2a9b2b9e9
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-07 21:52:53 +0200

    Create method to return all valid game codes

--- src/gameCode/gameCode.service.ts
+++ src/gameCode/gameCode.service.ts
@@ -3,6 +3,10 @@ import { GameCode, validGameCodes } from 'src/__types/gameCode';
 
 @Injectable()
 export class GameCodeService {
+  public getAllGameCodes(): GameCode[] {
+    return [...validGameCodes];
+  }
+
   public getGameCode(gameName: string): GameCode | null {
     if (!validGameCodes.includes(gameName as GameCode)) {
       return null;
