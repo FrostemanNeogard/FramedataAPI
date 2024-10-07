f0a360271d80dc4a8bd9002d305838c044346310
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-07 23:05:19 +0200

    Fix gameCodesService logic

--- src/gameCodes/gameCodes.service.ts
+++ src/gameCodes/gameCodes.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { GameCode, validGameCodes } from 'src/__types/gameCode';
 
 @Injectable()
 export class GameCodesService {
+  private readonly logger: Logger;
+
+  constructor() {
+    this.logger = new Logger();
+  }
+
   public getAllGameCodes(): GameCode[] {
     return [...validGameCodes];
   }
 
   public getGameCode(gameName: string): GameCode | null {
-    if (!validGameCodes.includes(gameName as GameCode)) {
+    if (![...validGameCodes].includes(gameName as GameCode)) {
+      this.logger.log(`Couldn't find gameCode for gameName: ${gameName}`);
       return null;
     }
 
-    return validGameCodes[gameName];
+    return validGameCodes[validGameCodes.indexOf(gameName as GameCode)];
   }
 }
