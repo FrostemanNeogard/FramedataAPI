e6985f40d47ec84a4e5d78abe97cac4f6ee7c252
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-10-07 21:42:32 +0200

    Extract all valid character codes to separate file

--- src/characterCode/characterCode.controller.ts
+++ src/characterCode/characterCode.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { CharacterCodeService } from './characterCode.service';
+import { GameCode } from 'src/__types/gameCode';
+import { CharacterCodeDto } from './dtos/characterCodeDto';
+import { GameCodeService } from 'src/gameCode/gameCode.service';
 
-@Controller('character-code')
+@Controller('charactercode')
 export class CharacterCodeController {
-  constructor(private characterCodeService: CharacterCodeService) {}
+  constructor(
+    private characterCodeService: CharacterCodeService,
+    private gameCodeService: GameCodeService,
+  ) {}
 
-  @Get(':characterName')
-  async formatCharacterName(@Param('characterName') name: string) {
-    return await this.characterCodeService.formatCharacterName(name);
+  @Get(':gameCode/:characterName')
+  public formatCharacterName(
+    @Param('gameCode') gameName: string,
+    @Param('characterName') characterName: string,
+  ): CharacterCodeDto {
+    const gameCode: GameCode | null =
+      this.gameCodeService.getGameCode(gameName);
+    if (gameCode == null) {
+      throw new NotFoundException("Couldn't find the given game.");
+    }
+
+    const characterCode = this.characterCodeService.getCharacterCode(
+      characterName,
+      gameCode,
+    );
+    if (characterCode == null) {
+      throw new NotFoundException("Couldn't find the given character.");
+    }
+
+    return new CharacterCodeDto(characterCode);
   }
 }
