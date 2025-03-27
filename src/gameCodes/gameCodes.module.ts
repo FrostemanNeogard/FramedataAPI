6e46d67748315e2691d6429c7817b27b4a8fbd91
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-03-27 21:41:59 +0100

    Add controller and exports

--- src/gameCodes/gameCodes.module.ts
+++ src/gameCodes/gameCodes.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { GameCodesController } from './gameCodes.controller';
 import { GameCodesService } from './gameCodes.service';
 
 @Module({
+  controllers: [GameCodesController],
   providers: [GameCodesService],
+  exports: [GameCodesService],
 })
 export class GameCodesModule {}
