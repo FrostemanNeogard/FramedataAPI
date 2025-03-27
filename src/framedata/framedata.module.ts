aeb60430754895637a3367ca19a291a3371021d7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-03-27 21:42:16 +0100

    Add additional fields

--- src/framedata/framedata.module.ts
+++ src/framedata/framedata.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FramedataController } from './framedata.controller';
 import { FramedataService } from './framedata.service';
+import { FrameData, FrameDataSchema } from './schemas/framedata.schema';
+import { CharacterCodesModule } from '../characterCodes/characterCodes.module';
 
-@Module({})
-export class FramedataModule {
-  imports: [FramedataService];
-}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: FrameData.name, schema: FrameDataSchema },
+    ]),
+    CharacterCodesModule,
+  ],
+  controllers: [FramedataController],
+  providers: [FramedataService],
+  exports: [FramedataService],
+})
+export class FramedataModule {}
