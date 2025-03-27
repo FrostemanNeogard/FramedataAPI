69a0732783a0571213eda7af54929fad650e109e
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-03-27 21:42:27 +0100

    Add additional fields

--- src/characterCodes/characterCodes.module.ts
+++ src/characterCodes/characterCodes.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CharacterCodesController } from './characterCodes.controller';
+import { CharacterCodesService } from './characterCodes.service';
+import { GameCodesModule } from '../gameCodes/gameCodes.module';
 
-@Module({})
+@Module({
+  imports: [GameCodesModule],
+  controllers: [CharacterCodesController],
+  providers: [CharacterCodesService],
+  exports: [CharacterCodesService],
+})
 export class CharacterCodesModule {}
