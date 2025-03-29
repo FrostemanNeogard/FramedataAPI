3183cb8c3a4ddbbae81fce8ca0566b724609edfe
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-03-29 09:21:35 +0100

    Create constructor for FramedataPatchDto

--- src/framedata/schemas/framedata.schema.ts
+++ src/framedata/schemas/framedata.schema.ts
@@ -1,10 +1,27 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { FramedataPatchDto } from 'src/__dtos/frameDataDto';
 
 export type FrameDataDocument = FrameData & Document;
 
 @Schema()
 export class FrameData {
+  constructor(framedataPatchDto: FramedataPatchDto) {
+    this.name = framedataPatchDto.name;
+    this.input = framedataPatchDto.input;
+    this.hit_level = framedataPatchDto.hit_level;
+    this.damage = framedataPatchDto.damage;
+    this.startup = framedataPatchDto.startup;
+    this.block = framedataPatchDto.block;
+    this.hit = framedataPatchDto.hit;
+    this.counter = framedataPatchDto.counter;
+    this.alternateInputs = framedataPatchDto.alternateInputs;
+    this.categories = framedataPatchDto.categories;
+    this.notes = framedataPatchDto.notes;
+    this.game = framedataPatchDto.game;
+    this.characterCode = framedataPatchDto.characterCode;
+  }
+
   @Prop({ required: true })
   name: string;
 
@@ -14,20 +31,20 @@ export class FrameData {
   @Prop({ required: true })
   hit_level: string;
 
-  @Prop({ required: true, type: [String, Number] })
-  damage: string | number;
+  @Prop({ required: true })
+  damage: string;
 
-  @Prop({ required: true, type: [String, Number] })
-  startup: string | number;
+  @Prop({ required: true })
+  startup: string;
 
-  @Prop({ required: true, type: [String, Number] })
-  block: string | number;
+  @Prop({ required: true })
+  block: string;
 
-  @Prop({ required: true, type: [String, Number] })
-  hit: string | number;
+  @Prop({ required: true })
+  hit: string;
 
-  @Prop({ required: true, type: [String, Number] })
-  counter: string | number;
+  @Prop({ required: true })
+  counter: string;
 
   @Prop({ required: true, type: [String] })
   alternateInputs: string[];
