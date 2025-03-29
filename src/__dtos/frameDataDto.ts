f4d5e6ad15c443fcb1867dab33fe4fcb104160b2
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-03-29 09:21:22 +0100

    Create FramedataPatchDto

--- src/__dtos/frameDataDto.ts
+++ src/__dtos/frameDataDto.ts
@@ -1,7 +1,10 @@
 import { GameCode } from 'src/__types/gameCode';
+import { FrameData } from 'src/framedata/schemas/framedata.schema';
 
 export class FramedataRequestDto {
   gameCode: GameCode;
   characterCode: string;
   input: string;
 }
+
+export class FramedataPatchDto extends FrameData {}
